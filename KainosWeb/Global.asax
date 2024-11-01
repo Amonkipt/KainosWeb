e5d449783bb39b399d821a47e86d2940f0cf5093
Amon Kiptoo <amonkiptoo76@gmail.com>
2024-11-01 11:19:49 -0700

    code changes

---
+++ KainosWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KainosWeb.MvcApplication" Language="C#" %>
---
+++ KainosWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KainosWeb.Startup))]
+namespace KainosWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
